74322315435361dabecbcb8b750907bf5507eb7e
Mischel <kandiahmischel@gmail.com>
2022-04-01 22:57:22 +1030

    Added FeedbackItem component - WIP

---
+++ src/components/FeedbackItem.jsx
@@ -0,0 +1,12 @@
+function FeedbackItem() {
+  return (
+    <div className='card'>
+      <div className='num-display'></div>
+      <div className='text-display'>
+        This is an example of a feedback item
+      </div>
+    </div>
+  )
+}
+
+export default FeedbackItem
