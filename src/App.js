6a34b646b7afa3e1db1c5442b3f7bd327fc2c897
Mischel <kandiahmischel@gmail.com>
2022-04-03 18:44:30 +0930

    Added form submit state management

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
+import {v4 as uuidv4} from 'uuid'
 import {useState} from 'react'
 import Header from './components/Header'
 import FeedbackList from './components/FeedbackList'
 import FeedbackData from './data/FeedbackData'
 import FeedbackStats from './components/FeedbackStats'
+import FeedbackForm from './components/FeedbackForm'
 
 export default function App({callback}) {
   const [feedback, setFeedback] = useState(FeedbackData)
+
+  const addFeedback = (newFeedback) => {
+    newFeedback.id = uuidv4()
+    setFeedback([newFeedback, ...feedback])
+    console.log(newFeedback)
+  }
 
   const deleteFeedbackItem = (id) => {
     if (window.confirm('Are you sure you want to delete this item?')) {
@@ -18,6 +26,7 @@ export default function App({callback}) {
     <div ref={callback}>
       <Header/>
       <div className='container'>
+        <FeedbackForm handleAdd={addFeedback}/>
         <FeedbackStats feedback={feedback}/>
         <FeedbackList feedback={feedback} handleDelete={deleteFeedbackItem}/>
       </div>
