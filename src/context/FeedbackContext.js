9f953f6e01111238348d38b93c7d83ae4e2bed57
Mischel <kandiahmischel@gmail.com>
2022-04-05 17:24:28 +0930

    Added sample feedback data into context

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -1,14 +1,11 @@
 import {createContext, useState} from 'react'
 import {v4 as uuidv4} from 'uuid'
+import FeedbackData from '../data/FeedbackData'
 
 const FeedbackContext = createContext()
 
 export const FeedbackProvider = ({children}) => {
-  const [feedback, setFeedback] = useState([{
-    id: '1',
-    text: 'this item is from context',
-    rating: 10
-  }])
+  const [feedback, setFeedback] = useState(FeedbackData)
 
   const addFeedbackItem = (newFeedback) => {
     newFeedback.id = uuidv4()
