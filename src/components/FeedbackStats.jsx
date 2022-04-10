98a9f575b981910cb8d2b3571a4921f1fee7d477
Mischel <kandiahmischel@gmail.com>
2022-04-10 15:12:11 +0930

    ratings no converted to number for average calc

--- src/components/FeedbackStats.jsx
+++ src/components/FeedbackStats.jsx
@@ -1,12 +1,12 @@
-import FeedbackContext from '../context/FeedbackContext'
 import {useContext} from 'react'
+import FeedbackContext from '../context/FeedbackContext'
 
 function FeedbackStats() {
   const {feedback} = useContext(FeedbackContext)
 
   // Calculate the average rating
   let average = feedback.reduce((acc, cur) => {
-    return acc + cur.rating
+    return acc + parseInt(cur.rating)
   }, 0) / feedback.length
 
   // average = average.toFixed(1).replace(/[.,]0$/, '')  Alternative to replace trailing numbers
